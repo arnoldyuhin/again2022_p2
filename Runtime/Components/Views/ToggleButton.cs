e607c25de6f9c732a99fb1f726e55e2cb0da414c
Arnold Chan <eagle17hk@gmail.com>
2025-12-17 18:42:44 +0800

    Updated fonts and graphic for p2 demo

--- Runtime/Components/Views/ToggleButton.cs
+++ Runtime/Components/Views/ToggleButton.cs
@@ -3,30 +3,31 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
-namespace Again.Runtime.Components.Views
-{
-    public class ToggleButton : MonoBehaviour, IToggleButton
-    {
+namespace Again.Runtime.Components.Views {
+    public class ToggleButton : MonoBehaviour, IToggleButton {
         public Color onColor = Color.white;
         public Color offColor = new(0.8f, 0.8f, 0.8f, 0.5f);
         private Button _button;
+        [SerializeField] private Image _targetImage;
         private Image _image;
         private bool _isOn;
 
-        private void Awake()
-        {
+        private void Awake() {
             _button = GetComponent<Button>();
             _image = GetComponent<Image>();
         }
 
-        public void Toggle(bool isOn)
-        {
+        public void Toggle(bool isOn) {
             _isOn = isOn;
-            _image.color = isOn ? onColor : offColor;
+            if (_targetImage != null) {
+                _targetImage.color = isOn ? onColor : offColor;
+            }
+            else {
+                _image.color = isOn ? onColor : offColor;
+            }
         }
 
-        public void SetOnClick(UnityAction<bool> onClick)
-        {
+        public void SetOnClick(UnityAction<bool> onClick) {
             _button.onClick.RemoveAllListeners();
             _button.onClick.AddListener(() => onClick(!_isOn));
         }
